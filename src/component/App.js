c003564561ee5ec911947e9ea0c4dfa677001f6f
gilang <gilang200398@gmail.com>
2020-01-16 14:26:34 +0700

    test

--- src/component/App.js
+++ src/component/App.js
@@ -1,63 +1,67 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';
 import Grid from './Grid';
 import Home from './home/Home';
 import '../style/style.css';
-import { BrowserRouter as Router,Link } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Link } from "react-router-dom";
 import $ from 'jquery';
+import News from './News';
 window.jQuery = $;
 window.$ = $;
 global.jQuery = $;
-
+ 
 class App extends Component {
-  render(){
-    return (
-      <Router>
-      <div className="header">
-        <div className="container warna">
-          <nav className="navbar navbar-expand-lg navbar-light warna">
-            <Link className="navbar-brand" to="/"> <img className="logo" src={require('../logo/logo.PNG')} alt="logo" /> </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="toggle-navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item dropdown">
-                  <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expadnded="false">
-                    Promo
-                  </Link>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <Link className="dropdown-item" to="#">Promo</Link>
-                    <Link className="dropdown-item" to="#">Partnership</Link>
-                  </div>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-extended="false">
-                    Paket & Add-On
-                  </Link>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <Link className="dropdown-item" to="#">Action</Link>
-                    <Link className="dropdown-item" to="#">Another Action</Link>
-                    <div className="dropdown-divider"></div>
-                    <Link className="dropdown-item" to="#">Something Else Here</Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link">Pusat Bantuan</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <div className="">
-            <Home/>
-          </div>
+    render(){
+      return(
+        <Router>
+        <div className="header">
+         <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light warna">
+                <Link className="navbar-brand" to=""> <img className="logo" src={require('../logo/logo.PNG')}  alt="logo"/></Link>
+                <button className="navbar-toggler toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+ 
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                      <li class="nav-item dropdown">
+                        <Link class="nav-link dropdown-toggle text-white" to="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          PROMO
+                        </Link>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <Link class="dropdown-item " to="/">PROMO</Link>
+                          <Link class="dropdown-item " to="/grid">Partnership</Link>
+                        </div>
+                      </li>
+                      <li className="nav-item dropdown">
+                          <Link className="nav-link dropdown-toggle text-white" to="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          PAKET & ADD-ON
+                          </Link>
+                          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <Link className="dropdown-item" to="">Action</Link>
+                          <Link className="dropdown-item" to="/news">Another action</Link>
+                          <div className="dropdown-divider"></div>
+                         
+                          </div>
+                      </li>
+                      <li className="nav-item">
+                          <Link className="nav-link text-white" to="/news">News</Link>
+                      </li>
+                    </ul>
+                </div>
+            </nav>             
+         </div>
+         <div className="">
+        
+ 
+            <Route path="/" exact component={Home}/>
+            <Route path="/grid"  component={Grid}/>
+            <Route path="/news" component={News}/>
+         </div>
+ 
         </div>
-
-      </div>
-      </Router>
-    )
-  }
+        </Router>
+      )
+    }
 }
-
+ 
 export default App;
--- src/component/Grid.jsx
+++ src/component/Grid.jsx
@@ -5,16 +5,16 @@ class Grid extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-6 test">
                     Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-6 test">
                     Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-6 test">
                     Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-6 test">
                     Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde
                     </div>
                 </div>
